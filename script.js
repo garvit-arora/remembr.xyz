a6f9eb883e11e6707b5436eb8b9d0c63442f1e7b
Garvit Arora <garvit.university@gmail.com>
2025-07-05 16:47:18 +0530

    fixing submission

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 const submit = document.getElementById('submit');
+const heading = document.getElementById('heading');
 function waitlist(){
     submit.addEventListener('click',function(){
-        submit.innerHTML = 'Joined Waitlist Successfully';
-        submit.style.width = "212px";
+        submit.innerHTML = 'Joining...';
+        // submit.style.width = "212px";
+        setTimeout(function(){
+            submit.style.background = "green";
+            submit.textContent = "JOINED !!";
+            submit.style.fontWeight = "bold";
+            const audio = document.getElementById('myAudio');
+            audio.play( );
+            setTimeout(function(){
+            submit.style.background = "linear-gradient(to right, #77562c 10%,rgb(8, 8, 8) 80%)";
+                submit.textContent = "Join Waitlist";
+            },7000)
+        },4000)
     })
 }
+waitlist();
